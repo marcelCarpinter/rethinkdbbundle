e7e238c0751dd19a67ff8bc00f792fb55ce99d12
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-11-06 17:56:10 -0300

    Added new method. Added phpdocblock

--- src/Query/Database.php
+++ src/Query/Database.php
@@ -18,6 +18,9 @@ class Database extends AbstractQuery
      */
     private $query;
 
+    /**
+     * @param RethinkInterface $rethink
+     */
     public function __construct(RethinkInterface $rethink)
     {
         parent::__construct($rethink);
@@ -25,6 +28,10 @@ class Database extends AbstractQuery
         $this->dbList();
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function dbCreate(string $name): Database
     {
         $this->query = new DbCreate($this->rethink, $name);
@@ -32,6 +39,10 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function dbDrop(string $name): Database
     {
         $this->query = new DbDrop($this->rethink, $name);
@@ -39,6 +50,9 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function dbList(): Database
     {
         $this->query = new DbList($this->rethink);
@@ -46,6 +60,9 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function tableList(): Database
     {
         $this->query = new TableList($this->rethink);
@@ -53,6 +70,10 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function tableCreate(string $name): Database
     {
         $this->query = new TableCreate($this->rethink, $name);
@@ -60,6 +81,10 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function tableDrop(string $name): Database
     {
         $this->query = new TableDrop($this->rethink, $name);
@@ -67,8 +92,20 @@ class Database extends AbstractQuery
         return $this;
     }
 
+    /**
+     * @return array
+     */
     public function toArray(): array
     {
         return $this->query->toArray();
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function tableExists(string $name): bool
+    {
+        return in_array($name, $this->tableList()->run()->getData());
     }
 }
