e9a7495a669be874fcc0bc73a54ec83bc0febc23
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-11-06 20:10:08 -0300

    Added test case folder to bundle. Updated namespace of test file

--- tests/RethinkDbTest.php
+++ tests/RethinkDbTest.php
@@ -1,8 +1,14 @@
 <?php
 
+namespace MCarpinter\RethinkDb\Tests;
+
 use MCarpinter\RethinkDb\RethinkDbBundle;
-use PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\TestCase;
 
 class RethinkDbTest extends TestCase
 {
+    public function testGetWords()
+    {
+        $this->assertTrue(true);
+    }
 }
