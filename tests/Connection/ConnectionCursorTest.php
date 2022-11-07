a1daa3fd5a2405bc3b903a74c81cfe8daabeceed
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-11-06 23:05:34 -0300

    Developed test cases

--- tests/Connection/ConnectionCursorTest.php
+++ tests/Connection/ConnectionCursorTest.php
@@ -2,7 +2,31 @@
 
 namespace MCarpinter\RethinkDb\Tests\Connection;
 
+use MCarpinter\RethinkDb\Message\MessageInterface;
+use MCarpinter\RethinkDb\Response\ResponseInterface;
+use MCarpinter\RethinkDb\Types\Response\ResponseType;
+use Exception;
+
 class ConnectionCursorTest extends ConnectionTestCase
 {
+    public function testRewindFromCursor()
+    {
+        $this->connect();
 
+        $message = \Mockery::mock(MessageInterface::class);
+        $message->shouldReceive('setOptions')->once();
+
+        $response = \Mockery::mock(ResponseInterface::class);
+        $response->shouldReceive('getType')->atLeast()->andReturn(ResponseType::SUCCESS_ATOM);
+
+        $this->setExpectations($response);
+
+        try {
+            $this->connection->rewindFromCursor($message);
+        } catch (Exception $e) {
+            $this->fail($e->getMessage());
+        }
+
+        $this->assertTrue(true);
+    }
 }
