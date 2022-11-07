efc64d820834ea0e7e36c3cdbbb65c595b7a79bf
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-11-06 23:26:34 -0300

    Dev ResponseTest class

--- tests/Response/ResponseTest.php
+++ tests/Response/ResponseTest.php
@@ -2,7 +2,36 @@
 
 namespace MCarpinter\RethinkDb\Tests\Response;
 
-class ResponseTest
-{
+use MCarpinter\RethinkDb\Response\Response;
+use MCarpinter\RethinkDb\Tests\RethinkDbTestCase;
+use Exception;
 
+class ResponseTest extends RethinkDbTestCase
+{
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testAccessors(): void
+    {
+        $type = 1;
+        $data = ['foo' => 'bar'];
+        $backtrace = [0 => [1 => []]];
+        $profile = [2 => [3 => []]];
+        $note = [4 => [5 => []]];
+
+        $response = new Response(
+            $type,
+            $data,
+            $backtrace,
+            $profile,
+            $note
+        );
+
+        $this->assertEquals($type, $response->getType());
+        $this->assertEquals($data, $response->getData());
+        $this->assertEquals($backtrace, $response->getBacktrace());
+        $this->assertEquals($profile, $response->getProfile());
+        $this->assertEquals($note, $response->getNote());
+    }
 }
